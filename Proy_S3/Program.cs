3fa7b7521e8c6d47a327e4041e9b2d369ac5508f
Jairo <Jairo>
2023-02-12 09:29:59 -0600

    Mi primer commit

---
+++ Proy_S3/Program.cs
@@ -0,0 +1,16 @@
+﻿int año;
+int edad;
+
+
+try
+{
+    Console.Write("En que año cumpleaños?");
+    año = Convert.ToInt32(Console.ReadLine());
+
+    edad = (2022 - año);
+    Console.WriteLine($"Waw no aparentas de {edad} años");
+}   catch(Exception ex)
+{
+    Console.WriteLine("Ups, no me entendiste, es un numero");
+    Console.WriteLine("La excepcion es: " + ex.Message);
+}
